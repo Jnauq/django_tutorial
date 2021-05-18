c91d9863ecf3e1985cfd2c3696e7435d459ccd47
= <=>
2021-05-17 23:21:48 -0700

    created and registered Post model, added entries to Post model

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Tom Hanks',
-        'title': 'Blog post 1',
-        'content': 'Testing first post!',
-        'date_posted': 'August 27, 2020'
-    },
-    {
-        'author': 'Michael B Jordan',
-        'title': 'Hey blog!',
-        'content': 'Testing blog app!',
-        'date_posted': 'November 1, 2020'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
