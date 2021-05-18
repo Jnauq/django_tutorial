203c55b63002e5ff8701e729600b14e72acf6de6
= <=>
2021-05-17 20:55:04 -0700

    added templates and css for blog app

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Tom Hanks',
+        'title': 'Blog post 1',
+        'content': 'Testing first post!',
+        'date_posted': 'August 27, 2020'
+    },
+    {
+        'author': 'Michael B Jordan',
+        'title': 'Hey blog!',
+        'content': 'Testing blog app!',
+        'date_posted': 'November 1, 2020'
+    },
+]
+
 
 def home(request):
-    return HttpResponse('<h2>Blog Homepage</h2>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', { 'title': 'About' })
